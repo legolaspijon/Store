4c243a0d3caaf76db3e214b9f60c729fb26befaa
Dima <legolaspijon@bigmir.net>
2016-05-14 16:06:11 +0000

    edit db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect("127.0.0.1", "root", "", "webstore");
+$db = mysqli_connect("37.252.123.142", "fomichev_pijon", "legolaspijon200512", "fomichev_store");
 
 if (!$db) {
     echo "Ошибка подключения к базе данных" . mysqli_connect_error();
